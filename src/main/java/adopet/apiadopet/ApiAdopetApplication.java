91a2f75f1a5771aea1283b7294e405bf6ae61ef3
lucasmrd <lucasvnx6@gmail.com>
2024-02-18 18:15:50 -0300

    primeiro commit

---
+++ src/main/java/adopet/apiadopet/ApiAdopetApplication.java
@@ -0,0 +1,13 @@
+package adopet.apiadopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdopetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdopetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adopet/apiadopet/ApiAdopetApplicationTests.java
@@ -0,0 +1,13 @@
+package adopet.apiadopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdopetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
