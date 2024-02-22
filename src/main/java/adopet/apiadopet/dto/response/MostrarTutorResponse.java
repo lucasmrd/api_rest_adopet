8e47db452fa03085a9d2e5a95099b9ff92658e11
lucasmrd <lucasvnx6@gmail.com>
2024-02-22 11:08:29 -0300

    Removendo ID do dto MostrarTutorResponse

--- src/main/java/adopet/apiadopet/dto/response/MostrarTutorResponse.java
+++ src/main/java/adopet/apiadopet/dto/response/MostrarTutorResponse.java
@@ -3,12 +3,11 @@ package adopet.apiadopet.dto.response;
 import adopet.apiadopet.entity.Tutor;
 
 public record MostrarTutorResponse(
-        Long id,
         String nome,
         String email,
         String senha) {
 
     public MostrarTutorResponse(Tutor tutor) {
-        this(tutor.getId(), tutor.getNome(), tutor.getEmail(), tutor.getSenha());
+        this(tutor.getNome(), tutor.getEmail(), tutor.getSenha());
     }
 }
