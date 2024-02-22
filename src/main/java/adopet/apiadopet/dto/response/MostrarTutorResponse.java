785ec15f37c8eea84c180976150abc1ba10e8e43
lucasmrd <lucasvnx6@gmail.com>
2024-02-22 11:30:17 -0300

    Adicionando ID no DTO MostarTutorResponse

--- src/main/java/adopet/apiadopet/dto/response/MostrarTutorResponse.java
+++ src/main/java/adopet/apiadopet/dto/response/MostrarTutorResponse.java
@@ -3,11 +3,12 @@ package adopet.apiadopet.dto.response;
 import adopet.apiadopet.entity.Tutor;
 
 public record MostrarTutorResponse(
+        Long id,
         String nome,
         String email,
         String senha) {
 
     public MostrarTutorResponse(Tutor tutor) {
-        this(tutor.getNome(), tutor.getEmail(), tutor.getSenha());
+        this(tutor.getId(), tutor.getNome(), tutor.getEmail(), tutor.getSenha());
     }
 }
