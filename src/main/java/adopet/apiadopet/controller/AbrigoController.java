bfc8c24df85c6591776040b6165cfedbc8ab88cf
Lucas Miranda <lucasvnx6@gmail.com>
2024-02-29 12:07:22 -0300

    Criado o método que cria um abrigo
    Retornando código 200 (created) junto com a uri e o dto response no corpo.

--- src/main/java/adopet/apiadopet/controller/AbrigoController.java
+++ src/main/java/adopet/apiadopet/controller/AbrigoController.java
@@ -1,12 +1,15 @@
 package adopet.apiadopet.controller;
 
 import adopet.apiadopet.dto.request.CriarAbrigoRequest;
+import adopet.apiadopet.dto.response.MostrarAbrigoResponse;
+import adopet.apiadopet.entity.Abrigo;
 import adopet.apiadopet.repository.AbrigoRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/abrigo")
@@ -14,8 +17,13 @@ public class AbrigoController {
 
     private AbrigoRepository abrigoRepository;
 
-    public ResponseEntity criar(@RequestBody @Valid CriarAbrigoRequest dtoRequest) {
-        
-        return ResponseEntity.ok().build();
+    public ResponseEntity criar(@RequestBody @Valid CriarAbrigoRequest dtoRequest, UriComponentsBuilder uriBuilder) {
+        var abrigo = new Abrigo(dtoRequest);
+        var dtoResponse = new MostrarAbrigoResponse(abrigo);
+        abrigoRepository.save(abrigo);
+
+        var uri = uriBuilder.path("/api/abrigo/{id}").buildAndExpand(abrigo.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dtoResponse);
     }
 }
