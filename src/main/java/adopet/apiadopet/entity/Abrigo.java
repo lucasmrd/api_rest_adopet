0d87c03a78c4567558fa9bf57a062b55ff763c4d
Lucas Miranda <lucasvnx6@gmail.com>
2024-02-29 13:01:51 -0300

    Criado método na entidade abrigo para atualizar os atributos

--- src/main/java/adopet/apiadopet/entity/Abrigo.java
+++ src/main/java/adopet/apiadopet/entity/Abrigo.java
@@ -1,5 +1,6 @@
 package adopet.apiadopet.entity;
 
+import adopet.apiadopet.dto.request.AtualizarAbrigoRequest;
 import adopet.apiadopet.dto.request.CriarAbrigoRequest;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -25,5 +26,16 @@ public class Abrigo {
     public Abrigo(CriarAbrigoRequest dtoRequest) {
         this.nome = dtoRequest.nome();
         this.telefone = dtoRequest.telefone();
+    }
+
+    public void atualizar(AtualizarAbrigoRequest att) {
+        if (!(att.nome() == null || att.nome().isEmpty() || att.nome().isBlank())) {
+            this.nome = att.nome();
+        }
+
+        if (!(att.telefone() == null || att.telefone().isEmpty() || att.telefone().isBlank())) {
+            this.telefone = att.telefone();
+        }
+
     }
 }
