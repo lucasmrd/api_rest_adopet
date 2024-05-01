876e24edd58e7534fa65090800d4bb1e2eaa53b4
Lucas Miranda <lucasvnx6@gmail.com>
2024-05-01 13:59:39 -0300

    Criado dto para obter tutor

---
+++ src/main/java/adopet/apiadopet/dto/obterDados/ObterDadosTutor.java
@@ -0,0 +1,15 @@
+package adopet.apiadopet.dto.obterDados;
+
+import adopet.apiadopet.entity.Tutor;
+import adopet.apiadopet.repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ObterDadosTutor {
+
+    @Autowired
+    private TutorRepository repository;
+
+    public Tutor get(Long id) {
+        return repository.getReferenceById(id);
+    }
+}
