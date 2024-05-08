7c8c747eb4cb1a84bfadd399bea84f2dce6ac0fb
Lucas Miranda <lucasvnx6@gmail.com>
2024-05-08 11:01:28 -0300

    Criada a classe para tratar exceptions

---
+++ src/main/java/adopet/apiadopet/infra/TratadorDeErros.java
@@ -0,0 +1,15 @@
+package adopet.apiadopet.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity erro404() {
+        return ResponseEntity.notFound().build();
+    }
+}
