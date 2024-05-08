ec78776a781f7f379536f87b914a3054925527c1
Lucas Miranda <lucasvnx6@gmail.com>
2024-05-08 15:33:36 -0300

    Devolvendo token autenticado

--- src/main/java/adopet/apiadopet/controller/AutenticacaoController.java
+++ src/main/java/adopet/apiadopet/controller/AutenticacaoController.java
@@ -1,6 +1,9 @@
 package adopet.apiadopet.controller;
 
 import adopet.apiadopet.dto.request.EfetuarLoginRequest;
+import adopet.apiadopet.dto.response.DadosJWTResponse;
+import adopet.apiadopet.entity.Usuario;
+import adopet.apiadopet.service.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +21,15 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid EfetuarLoginRequest dtoLogin) {
         var token = new UsernamePasswordAuthenticationToken(dtoLogin.login(), dtoLogin.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        var jwt = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosJWTResponse(jwt));
     }
 }
