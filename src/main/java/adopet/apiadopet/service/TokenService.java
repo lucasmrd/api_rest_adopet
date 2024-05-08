99aa418ba4f2fd1e4464067c9e49cbce5ec7d830
Lucas Miranda <lucasvnx6@gmail.com>
2024-05-08 15:32:56 -0300

    Criada a classe TokenService para gerar o token JWT

---
+++ src/main/java/adopet/apiadopet/service/TokenService.java
@@ -0,0 +1,37 @@
+package adopet.apiadopet.service;
+
+import adopet.apiadopet.entity.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${api.token.secret}")
+    private String secret;
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("adopet")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar JWT", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusMonths(1).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
