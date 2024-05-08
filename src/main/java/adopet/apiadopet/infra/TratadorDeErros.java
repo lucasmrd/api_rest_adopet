63c8c0c837ff5837cc9ed2aacc86566d7610a2a9
Lucas Miranda <lucasvnx6@gmail.com>
2024-05-08 11:25:17 -0300

    Tratando erro 400 na classe TratadorDeErros

--- src/main/java/adopet/apiadopet/infra/TratadorDeErros.java
+++ src/main/java/adopet/apiadopet/infra/TratadorDeErros.java
@@ -2,8 +2,12 @@ package adopet.apiadopet.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class TratadorDeErros {
@@ -11,5 +15,21 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity erro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity erro400(MethodArgumentNotValidException e) {
+        var erros = e.getFieldErrors();
+        erros.stream()
+                .map(DadosErroValidacao::new)
+                .toList();
+
+        return ResponseEntity.badRequest().body(erros);
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
