3b7f9e7a2825f514bc5d6bbc9a790d2847e9dde4
Lucas Miranda <lucasvnx6@gmail.com>
2024-05-08 13:32:58 -0300

    Implementado método findByLogin

--- src/main/java/adopet/apiadopet/repository/UsuarioRepository.java
+++ src/main/java/adopet/apiadopet/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package adopet.apiadopet.repository;
 
 import adopet.apiadopet.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
 }
