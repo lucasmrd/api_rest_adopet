793b1b1205a02a58d5959bf2b86c069e896f92ee
Lucas Miranda <lucasvnx6@gmail.com>
2024-05-08 11:37:45 -0300

    Removendo import não utilizado

--- src/main/java/adopet/apiadopet/infra/TratadorDeErros.java
+++ src/main/java/adopet/apiadopet/infra/TratadorDeErros.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.stream.Collectors;
-
 @RestControllerAdvice
 public class TratadorDeErros {
 
