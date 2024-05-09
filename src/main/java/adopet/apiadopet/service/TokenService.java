63baa03c362ec5420bab8ae65744bedb3b96eb48
Lucas Miranda <lucasvnx6@gmail.com>
2024-05-09 15:55:20 -0300

    Criado o método getSubject para validar um token jwt

--- src/main/java/adopet/apiadopet/service/TokenService.java
+++ src/main/java/adopet/apiadopet/service/TokenService.java
@@ -4,6 +4,7 @@ import adopet.apiadopet.entity.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -28,6 +29,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("adopet")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
